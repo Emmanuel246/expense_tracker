35f77d98b6047ed5183100d112a7b72746afdf2e
Makanjuola Emmanuel <makanjuolaiseoluwa1@gmail.com>
2024-10-25 09:43:28 +0100

    Theme Data

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
 
+var KColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(225, 96, 59, 181));
 void main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
+    theme: ThemeData().copyWith(
+        colorScheme: KColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: KColorScheme.onPrimaryContainer,
+          foregroundColor: KColorScheme.primaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+            color: KColorScheme.secondaryContainer,
+            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: KColorScheme.primaryContainer,
+          ),
+        ),   
+        textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: KColorScheme.onSecondaryContainer,
+            fontSize: 18
+          )
+        )
+         
+            ),
     debugShowCheckedModeBanner: false,
-    home: Expenses(),
+    home: const Expenses(),
   ));
 }
